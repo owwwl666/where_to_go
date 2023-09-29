ac50ad323921c7ac4e70c5a8563cfed9422c7ef9
owwwl666 <zaharsalam228@gmail.com>
2023-09-29 21:17:36 +1000

    Add model for images

--- places/models.py
+++ places/models.py
@@ -7,3 +7,12 @@ class Place(models.Model):
     description_long = models.TextField(verbose_name='Полное описание')
     longitude = models.FloatField(verbose_name='Долгота местоположения')
     latitude = models.FloatField(verbose_name='Широта местоположения')
+
+    def __str__(self):
+        return self.title
+
+
+class PlaceImage(models.Model):
+    image = models.ImageField(verbose_name='Изображение')
+    number = models.PositiveIntegerField(verbose_name='Номер изображения')
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='places', verbose_name='Место')
