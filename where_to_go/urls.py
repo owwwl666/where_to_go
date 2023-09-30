458d44f0ab291835c7a263d5abea9e1b82622784
owwwl666 <zaharsalam228@gmail.com>
2023-09-30 21:41:34 +1000

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import show_map
+from places import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_map)
+    path('', views.show_map)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
