bf342d0be7039f93b0989164f1ec63224cebfc12
owwwl666 <zaharsalam228@gmail.com>
2023-10-17 22:21:13 +1000

    made the description_long field editable using HTML

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     description_short = models.TextField(null=True, blank=True, verbose_name='Короткое описание')
-    description_long = models.TextField(verbose_name='Полное описание')
+    description_long = HTMLField(verbose_name='Полное описание')
     longitude = models.FloatField(verbose_name='Долгота местоположения')
     latitude = models.FloatField(verbose_name='Широта местоположения')
 
