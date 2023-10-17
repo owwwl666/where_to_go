6445af397fff455b3cff66146fa3daf2b8926d86
owwwl666 <zaharsalam228@gmail.com>
2023-10-17 20:47:43 +1000

    Update admin.py
    added the ability to receive information about photos from the places page in the admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title']
+    inlines = [PlaceImageInline]
 
-
-@admin.register(PlaceImage)
-class PlaceImageAdmin(admin.ModelAdmin):
-    list_display = ['number', 'place']
