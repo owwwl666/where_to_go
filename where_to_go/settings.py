24a366c77953846339fc6f32ba7c616ef0e0e701
owwwl666 <zaharsalam228@gmail.com>
2023-10-25 00:40:09 +1000

    Update settings.py
    made the adjustment ALLOWED_HOSTS. set DEBUG to default value.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['*'])
 
 # Application definition
 
