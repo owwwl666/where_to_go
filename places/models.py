ede0f9d32e20f4527dc4836d3cfa68e735969fcf
owwwl666 <zaharsalam228@gmail.com>
2023-10-25 22:19:45 +1000

    made fields optional

--- places/models.py
+++ places/models.py
@@ -4,9 +4,9 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Название',unique=True)
+    title = models.CharField(max_length=200, verbose_name='Название', unique=True)
     short_description = models.TextField(null=True, blank=True, verbose_name='Короткое описание')
-    long_description = HTMLField(verbose_name='Полное описание')
+    long_description = HTMLField(verbose_name='Полное описание', null=True, blank=True)
     longitude = models.FloatField(verbose_name='Долгота местоположения')
     latitude = models.FloatField(verbose_name='Широта местоположения')
 
@@ -21,7 +21,8 @@ class Place(models.Model):
 
 class PlaceImage(models.Model):
     image = models.ImageField(verbose_name='Изображение')
-    number = models.PositiveIntegerField(verbose_name='Номер изображения', db_index=True)
+    number = models.PositiveIntegerField(verbose_name='Номер изображения', null=True, blank=True, default=0,
+                                         db_index=True)
     place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images', verbose_name='Место')
 
     def __str__(self):
